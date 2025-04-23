2e48cf944e2bc52924d00c8191b5e83855661764
jinseok2101 <jslee2101@naver.com>
2025-04-23 23:58:08 +0900

    Deployment

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,10 @@
 import Movie from "../../components/movie";
 import styles from "../../styles/home.module.css";
+import { API_URL } from "../constants";
 
 export const metadata = {
   title: "Home",
 };
-
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   // await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -29,3 +28,5 @@ export default async function HomePage() {
     </div>
   );
 }
+
+export const runtime = "edge";
